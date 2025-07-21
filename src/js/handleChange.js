1bee1f4fd5ceba2f9a3f492195e833586e37f759
romavsterg <romann.mikhaylov@gmail.com>
2025-07-21 22:42:07 +0300

    feat: добавил изменение должности, работы, языков

--- src/js/handleChange.js
+++ src/js/handleChange.js
@@ -5,19 +5,36 @@ export const handleClick = e => {
 	const x = element.offsetLeft + e.target.offsetWidth / 2
 	const y = element.offsetTop
 
+	localStorage.setItem('currentDataId', element.getAttribute('data-id'))
+
 	openPopup(element.id, x, y)
 }
 
 export const handleChange = (id, newData) => {
+	const input = document.querySelector(`#${id}-input`)
+
 	switch (id) {
 		case 'avatar':
 			document.querySelector('#avatar').style.backgroundImage =
 				`url(${newData})`
-
 			break
-		case 'name':
-			document.querySelector('#name').textContent = newData
+		case 'lang-progress':
+			document
+				.querySelector(`[data-id=${localStorage.getItem('currentDataId')}]`)
+				.style.setProperty('--level', `${newData * 10}%`)
+			input.value = '0'
+			input.style = ''
+			document.querySelector(`#save-${id}`).classList.add('disabled')
+			document.querySelector(`#save-${id}`).disabled = true
+			break
+		default:
+			document.querySelector(
+				// eslint-disable-next-line prettier/prettier
+				`[data-id="${localStorage.getItem('currentDataId')}"]`
+			).textContent = newData
+			input.value = ''
 			break
 	}
+
 	document.querySelector('.popup').classList.remove('active')
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import { handleChange, handleClick } from './handleChange.js'
 
+const inputs = ['name', 'bio-heading', 'job', 'lang']
+
 document.addEventListener('click', e => {
 	if (
 		e.target.classList.contains('popup') ||
-		e.target.parentNode.parentNode.classList.contains('popup')
+		e.target.parentNode.classList.contains('popup') ||
+		e.target.parentNode.parentNode.classList.contains('popup') ||
+		e.target.parentNode.parentNode.parentNode.classList.contains('popup')
 	) {
 		return
 	}
@@ -25,20 +29,43 @@ document.querySelector('#avatar-input').addEventListener('change', e => {
 	}
 })
 
-document.querySelector('#name-input').addEventListener('input', e => {
-	document
-		.querySelector('#name-input')
-		.classList.toggle('error', !e.target.value)
+document.querySelector('#lang-progress-input').addEventListener('input', e => {
+	const slider = e.target
+
+	const value = slider.value * 10
+
+	slider.style.background = `linear-gradient(to right, #28d979 ${value}%, transparent ${value}%)`
 
 	document
-		.querySelector('#save-name')
-		.classList.toggle('disabled', !e.target.value)
+		.querySelector(`#save-lang-progress`)
+		.classList.toggle('disabled', e.target.value === '0')
 
-	document.querySelector('#save-name').disabled = !e.target.value
+	document.querySelector(`#save-lang-progress`).disabled =
+		e.target.value === '0'
 })
 
-document.querySelector('#save-name').addEventListener('click', () => {
-	const newValue = document.querySelector('#name-input').value
+document.querySelector(`#save-lang-progress`).addEventListener('click', () => {
+	const newValue = document.querySelector(`#lang-progress-input`).value
 
-	handleChange('name', newValue)
+	handleChange('lang-progress', newValue)
+})
+
+inputs.forEach(inputId => {
+	document.querySelector(`#${inputId}-input`).addEventListener('input', e => {
+		document
+			.querySelector(`#${inputId}-input`)
+			.classList.toggle('error', !e.target.value)
+
+		document
+			.querySelector(`#save-${inputId}`)
+			.classList.toggle('disabled', !e.target.value)
+
+		document.querySelector(`#save-${inputId}`).disabled = !e.target.value
+	})
+
+	document.querySelector(`#save-${inputId}`).addEventListener('click', () => {
+		const newValue = document.querySelector(`#${inputId}-input`).value
+
+		handleChange(inputId, newValue)
+	})
 })
